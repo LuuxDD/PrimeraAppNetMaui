763a24f5f374c3269abaed66f0c46914ea9f2717
LeandroLJ <leandrolazzaroni@live.com>
2024-06-14 13:48:19 -0300

    viernes

--- PrimeraAppNetMaui/PrimeraPagina.xaml.cs
+++ PrimeraAppNetMaui/PrimeraPagina.xaml.cs
@@ -2,8 +2,21 @@ namespace PrimeraAppNetMaui;
 
 public partial class PrimeraPagina : ContentPage
 {
-	public PrimeraPagina()
+    private object bntInstituto;
+
+    public PrimeraPagina()
 	{
 		InitializeComponent();
-	}
+        bntInstituto.Clicked += btnInstituto_Clicked;
+        btnCarrera.Clicked += async (s, a ) => 
+        {
+
+        }
+
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		await Application.Current.MainPage.DisplayAlert("Ventana de Mensaje", "Hola Mundo y bienvenido a netMaui", "ok");
+    }
 }
